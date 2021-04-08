2e7334f95fc58be430ba0f342a52eba6e68c7458
DG <dg@email.com>
2021-04-08 16:14:41 +0530

    layout finalized

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import './App.css';
-import Board from 'react-legra'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Happy from './components/1-happy'
+import { CardDeck, Card, Button } from 'react-bootstrap'
 
 function App() {
-
-  const vertices = [
-    [10, 8],
-    [8, 11],
-    [12, 11]
-  ]
-
   return (
     <div className="App">
-      <header className="App-header">
-        <Board width="600" height="600">
-          <Board.Circle center={[10, 10]} radius={8} options={{ filled: true, color: 'yellow' }} />
-          <Board.Rectangle start={[6, 6]} width={2} height={2} options={{ filled: true, color: 'green' }} />
-          <Board.Rectangle start={[13, 6]} width={2} height={2} options={{ filled: true, color: 'green' }} />
-          <Board.Polygon vertices={vertices} options={{ filled: true, color: 'red' }} />
-          <Board.QuadraticCurve
-            from={[5, 13]}
-            to={[15, 13]}
-            controlPoint={[10, 22]}
-          />
-        </Board>
-      </header>
+      <CardDeck>
+        <Card className="text-center">
+          <Card.Header as="h5">Featured</Card.Header>
+          <Card.Body>
+            <Happy />
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
+        <Card className="text-center">
+          <Card.Header as="h5">Featured</Card.Header>
+          <Card.Body>
+            <Happy />
+          </Card.Body>
+        </Card>
+        <Card className="text-center">
+          <Card.Header as="h5">Featured</Card.Header>
+          <Card.Body>
+            <Happy />
+          </Card.Body>
+        </Card>
+        <Card className="text-center">
+          <Card.Header as="h5">Featured</Card.Header>
+          <Card.Body>
+            <Happy />
+          </Card.Body>
+        </Card>
+      </CardDeck>
+
     </div>
   );
 }
---
+++ src/components/1-happy.js
@@ -0,0 +1,24 @@
+import Board from 'react-legra'
+
+const Happy = () => {
+    const vertices = [
+        [10, 8],
+        [8, 11],
+        [12, 11]
+      ]
+    return (
+        <Board width="500" height="500" style={{width: '100%', height: 'auto'}}>
+          <Board.Circle center={[10, 10]} radius={9} options={{ filled: true, color: 'yellow' }} />
+          <Board.Rectangle start={[6, 6]} width={2} height={2} options={{ filled: true, color: 'green' }} />
+          <Board.Rectangle start={[13, 6]} width={2} height={2} options={{ filled: true, color: 'green' }} />
+          <Board.Polygon vertices={vertices} options={{ filled: true, color: 'red' }} />
+          <Board.QuadraticCurve
+            from={[5, 13]}
+            to={[15, 13]}
+            controlPoint={[10, 19]}
+          />
+        </Board>
+    );
+}
+
+export default Happy;
