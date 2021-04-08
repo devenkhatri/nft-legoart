efa110f9c8f321bad10cc8a2dbed55286223ac1a
DG <dg@email.com>
2021-04-08 15:04:25 +0530

    First smiley created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from 'react-legra'
 
 function App() {
+
+  const vertices = [
+    [10, 8],
+    [8, 11],
+    [12, 11]
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Board width="600" height="600">
+          <Board.Circle center={[10, 10]} radius={8} options={{ filled: true, color: 'yellow' }} />
+          <Board.Rectangle start={[6, 6]} width={2} height={2} options={{ filled: true, color: 'green' }} />
+          <Board.Rectangle start={[13, 6]} width={2} height={2} options={{ filled: true, color: 'green' }} />
+          <Board.Polygon vertices={vertices} options={{ filled: true, color: 'red' }} />
+          <Board.QuadraticCurve
+            from={[5, 13]}
+            to={[15, 13]}
+            controlPoint={[10, 22]}
+          />
+        </Board>
       </header>
     </div>
   );
